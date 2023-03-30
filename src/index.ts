620c82287c6ee7624d8d69a3c664af13960d5da2
hopeVaughn <hopevaughnwarren@gmail.com>
2023-03-29 21:33:31 -0700

    update index and router

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,31 @@
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import path from 'path';
-
+import express from 'express'
+import router from './router'
+import morgan from 'morgan'
+import cors from 'cors'
+import { protect } from './modules/auth';
+import { createNewUser, signin } from "./handlers/user";
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+// remember to put all your middleware above your routes in order for them to be able to access the middleware functionality
+//---------Start of MiddleWare----------------------------
+// this allows clients to access the api
+app.use(cors())
+// morgan allows us to receive data on the requests we send. The response can be found in the logs
+app.use(morgan('dev'))
+// express.json allows us to receive json back to our server
+app.use(express.json())
+//express.urlencoded allows a client to add things like a query string or a parameter and have it encoded properly.
+app.use(express.urlencoded({ extended: true }))
+//---------End of MiddleWare----------------------------
 
-app.get('/api/hello', (req: Request, res: Response) => {
-  res.send({ message: 'Hello from the server!' });
-});
 
-// Serve static assets from the React app
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.get('/', (req, res) => {
+  console.log('hello from express');
+  res.status(200)
+  res.json({ message: "hello" })
+})
 
-// Handles any requests that don't match the ones above
-app.get('*', (req: Request, res: Response) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.use('/api', protect, router)
+app.post('/user', createNewUser)
+app.post('/signin', signin)
+export default app
